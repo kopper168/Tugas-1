e7c85dfb765960d9f18791aa0fd77ec1b1e7bd20
Sodikin <sodikin@dana.id>
2021-11-01 00:54:48 +0700

    Soal 2

--- Soal[2].py
+++ Soal[2].py
@@ -1,5 +1,4 @@
 jari_jari = input("Masukan jari-jari lingkaran (cm) :")
 luas = 22/7*int(jari_jari)**2
-# print(f"Luas Lingkaran dengan Jari-jari {jari_jari} cm adalah {luas} cm\u00b2”)
-# print("Luas Lingkaran dengan Jari-jari {} cm adalah {} cm\u00b2”.fo)
 print("Luas Lingkaran dengan Jari-jari {} cm adalah {} cm\u00b2".format(jari_jari,luas))
+print("Luas Lingkaran dengan Jari-jari {} cm adalah {:.2f} cm\u00b2".format(jari_jari,luas))
