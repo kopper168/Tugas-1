721e54b35b9a0baa44dde56ab5a3dc1de0fd81ff
Sodikin <sodikin@dana.id>
2021-11-01 00:37:55 +0700

    latihan soal 1

---
+++ Soal[1].py
@@ -0,0 +1,4 @@
+nama = input("Nama : ")
+umur = input("Umur : ")
+tinggi = input("Tinggi : ")
+print(f"Nama saya {nama}, umur saya {umur} tahun, dan tinggi saya {tinggi} cm")
---
+++ Soal[2].py
@@ -0,0 +1,5 @@
+jari_jari = input("Masukan jari-jari lingkaran (cm) :")
+luas = 22/7*int(jari_jari)**2
+# print(f"Luas Lingkaran dengan Jari-jari {jari_jari} cm adalah {luas} cm\u00b2”)
+# print("Luas Lingkaran dengan Jari-jari {} cm adalah {} cm\u00b2”.fo)
+print("Luas Lingkaran dengan Jari-jari {} cm adalah {} cm\u00b2".format(jari_jari,luas))
